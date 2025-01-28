e500045512cdaf8caab9f87626f803f6917beb39
paoue <grappercosmo@gmail.com>
2025-01-28 19:11:57 +0100

    couleur du background mise à jour

--- app/favoris.tsx
+++ app/favoris.tsx
@@ -1,5 +1,17 @@
-import { SafeAreaView } from "react-native";
+import { colors } from "@/constants/color";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function Favoris() {
-  return <SafeAreaView></SafeAreaView>;
+  const backgroundColor = colors.background.purple;
+  return (
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: backgroundColor }]}
+    ></SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
