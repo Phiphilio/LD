fe54853e626d0a596776bf47bca3f0dfb61c1c9b
paoue <grappercosmo@gmail.com>
2025-01-28 19:12:38 +0100

    ajout d'une vue pour la barre de recherche

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View, SafeAreaView } from "react-native";
+import { SearchBar } from "@/components/searchBar";
+import { colors } from "@/constants/color";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 
 export default function Index() {
+  const backgroundColor = colors.background.purple;
   return (
     <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[styles.container, { backgroundColor: backgroundColor }]}
     >
-      <Text>début de LD</Text>
+      <View style={styles.header}>
+        <SearchBar />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 35,
+    paddingBottom: 35,
+  },
+});
