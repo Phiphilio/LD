cc99da4f82318285e82b0b834d3acc9a310c1417
paoue <grappercosmo@gmail.com>
2025-01-28 18:21:52 +0100

    modification du type de navigation pour les écrans de ce niveau et mise en place des styles/icones

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,39 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { colors } from "@/constants/color";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorIcone = colors.background.purple;
+  const colorInactiveIcon = colors.Card.lightPurple;
+  const colorBackgroundTabBar = colors.background.gray;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colorIcone, // couleur de l'icone actif
+        tabBarInactiveTintColor: colorInactiveIcon, // couleur de l'icone inactif
+        tabBarStyle: { backgroundColor: colorBackgroundTabBar }, // couleur du background de la tabBar
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Recettes",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 size={28} name="cocktail" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favoris"
+        options={{
+          title: "Favoris",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="heart" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
