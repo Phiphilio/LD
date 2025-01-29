b0132521a7b0a5133c7d21705ab76edf69bce930
paoue <grappercosmo@gmail.com>
2025-01-29 13:16:20 +0100

    mise en plae d'un hook pour récupérer les data de l'api

---
+++ hooks/useFecthData.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export function useFetchData(url: string) {
+  const [data, setData] = useState<object[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result?.drinks || []); // On stocke les boissons
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
