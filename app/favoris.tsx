e890bf965f8c75ce1193b2b4858a3684ddc540e6
paoue <grappercosmo@gmail.com>
2025-02-02 20:13:56 +0100

    je mets en place un header

--- app/favoris.tsx
+++ app/favoris.tsx
@@ -1,17 +1,33 @@
+import { ThemedText } from "@/components/ThemedText";
 import { colors } from "@/constants/color";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 export default function Favoris() {
   const backgroundColor = colors.background.purple;
+  const header = colors.Card.lightPurple;
   return (
     <SafeAreaView
       style={[styles.container, { backgroundColor: backgroundColor }]}
-    ></SafeAreaView>
+    >
+      <View style={[styles.header, { backgroundColor: header }]}>
+        <ThemedText variant="title"> Favoris </ThemedText>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    paddingTop: 15,
+    paddingBottom: 25,
+    borderRadius: 24,
+    width: 300,
   },
 });
