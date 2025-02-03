3aa42f809ae03123597a18798cda01696e90f06b
paoue <grappercosmo@gmail.com>
2025-02-03 18:40:44 +0100

    mise à jour globale du porjet suite à l'installation de la librairie react-native-sortables

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
@@ -8,34 +10,36 @@ export default function RootLayout() {
   const colorIcone = colors.Card.lightPurple;
   const colorBackgroundTabBar = colors.background.gray;
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: colorIcone, // couleur de l'icone actif
-        tabBarInactiveTintColor: colorInactiveIcon, // couleur de l'icone inactif
-        tabBarStyle: { backgroundColor: colorBackgroundTabBar }, // couleur du background de la tabBar
-      }}
-    >
-      <Tabs.Screen
-        name="index" // nom du fichier qui doit servir d'écran
-        options={{
-          title: "Recettes",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 size={28} name="cocktail" color={color} />
-          ),
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: colorIcone, // couleur de l'icone actif
+          tabBarInactiveTintColor: colorInactiveIcon, // couleur de l'icone inactif
+          tabBarStyle: { backgroundColor: colorBackgroundTabBar }, // couleur du background de la tabBar
         }}
-      />
-      <Tabs.Screen
-        name="favoris"
-        options={{
-          title: "Favoris",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="heart" color={color} />
-          ),
-        }}
-      />
+      >
+        <Tabs.Screen
+          name="index" // nom du fichier qui doit servir d'écran
+          options={{
+            title: "Recettes",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 size={28} name="cocktail" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="favoris"
+          options={{
+            title: "Favoris",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="heart" color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen name="cocktail" options={{ href: null }} />
-    </Tabs>
+        <Tabs.Screen name="cocktail" options={{ href: null }} />
+      </Tabs>
+    </GestureHandlerRootView>
   );
 }
