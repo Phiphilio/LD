ea53b64639a1012eec67ca7274e052b60f37b4b4
paoue <grappercosmo@gmail.com>
2025-02-03 19:13:03 +0100

    j'ai mis en place une liste dynamique

--- app/favoris.tsx
+++ app/favoris.tsx
@@ -2,14 +2,26 @@ import { ThemedText } from "@/components/ThemedText";
 import { useCallback } from "react";
 import { colors } from "@/constants/color";
 import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import Animated, { useAnimatedRef } from "react-native-reanimated";
 import type { SortableGridRenderItem } from "react-native-sortables";
 import Sortable from "react-native-sortables";
 
-const DATA = Array.from({ length: 12 }, (_, index) => `Item ${index + 1}`);
+const DATA = Array.from({ length: 30 }, (_, index) => `Item ${index + 1}`);
 
 export default function Favoris() {
   const backgroundColor = colors.background.purple;
   const header = colors.Card.lightPurple;
+
+  const scrollableRef = useAnimatedRef<Animated.ScrollView>();
+
+  const renderItem = useCallback<SortableGridRenderItem<string>>(
+    ({ item }) => (
+      <View style={styles.card}>
+        <Text style={styles.text}>{item}</Text>
+      </View>
+    ),
+    []
+  );
   return (
     <SafeAreaView
       style={[styles.container, { backgroundColor: backgroundColor }]}
@@ -17,18 +29,25 @@ export default function Favoris() {
       <View style={[styles.header, { backgroundColor: header }]}>
         <ThemedText variant="title"> Favoris </ThemedText>
       </View>
-      <Sortable.Grid
-        columns={3}
-        data={DATA}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <Text>{item}</Text>
-          </View>
-        )}
-        rowGap={10}
-        columnGap={10}
-        hapticsEnabled={false}
-      />
+      <Animated.ScrollView
+        contentContainerStyle={styles.contentContainer}
+        ref={scrollableRef}
+      >
+        <Sortable.Grid
+          columns={2}
+          data={DATA}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.text}>{item}</Text>
+            </View>
+          )}
+          rowGap={10}
+          columnGap={10}
+          scrollableRef={scrollableRef}
+          showDropIndicator={true}
+          hapticsEnabled={false}
+        />
+      </Animated.ScrollView>
     </SafeAreaView>
   );
 }
@@ -42,6 +61,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 20,
+    marginBottom: 50,
     paddingTop: 15,
     paddingBottom: 25,
     borderRadius: 24,
@@ -53,5 +73,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: "center",
     justifyContent: "center",
+  },
+  contentContainer: {
+    width: 350,
+    padding: 10,
+    //backgroundColor: "yellow",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
