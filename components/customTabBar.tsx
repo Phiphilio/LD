8cf2696043ef34c34e8b203a4f2e501d90335156
paoue <grappercosmo@gmail.com>
2025-02-08 00:54:37 +0100

    ajout d'une tabBar customisable

---
+++ components/customTabBar.tsx
@@ -0,0 +1,43 @@
+import { colors } from "@/constants/color";
+import { useRouter, useSegments } from "expo-router";
+import { View, Pressable, Text, StyleSheet } from "react-native";
+
+export function CustomTabBar() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  const purple = colors.background.purple;
+
+  const tabs = [
+    { name: "breakfast", route: "/" }, // ↪ Correspond à `app/index.tsx`
+    { name: "dinner", route: "/dinner" }, // ↪ Correspond à `app/dinner.tsx`
+    { name: "cocktails", route: "/cocktails" }, // ↪ Correspond à `app/cocktails.tsx`
+  ];
+
+  return (
+    <View style={styles.container}>
+      {tabs.map((tab, index) => (
+        <Pressable key={index} onPress={() => router.push(tab?.route)}>
+          <Text
+            style={{
+              color: segments.includes(tab.route.replace("/", ""))
+                ? purple
+                : "black",
+            }}
+          >
+            {tab.name}
+          </Text>
+        </Pressable>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 20,
+  },
+});
