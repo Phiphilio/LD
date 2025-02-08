9dd1df4a86b01607c560c4a6b6c048370570acad
paoue <grappercosmo@gmail.com>
2025-02-08 13:43:11 +0100

    dernier on change la couleur du background

--- app/dinner.tsx
+++ app/dinner.tsx
@@ -1,8 +1,10 @@
+import { colors } from "@/constants/color";
 import { View, StyleSheet, Text } from "react-native";
 
 export default function Dinner() {
+  const backgroundColor = colors.background.glassPurple;
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
       <Text>test</Text>
     </View>
   );
@@ -11,6 +13,5 @@ export default function Dinner() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
   },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
+import { colors } from "@/constants/color";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function index() {
+  const backgroundColor = colors.background.glassPurple;
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
       <Text>test</Text>
     </View>
   );
@@ -11,6 +13,5 @@ export default function index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
   },
 });
