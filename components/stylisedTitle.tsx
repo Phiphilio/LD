d3725537979e06c403d6507d43f68304583c8361
paoue <grappercosmo@gmail.com>
2025-02-10 14:47:38 +0100

    le style est conforme à la maquête

--- components/stylisedTitle.tsx
+++ components/stylisedTitle.tsx
@@ -6,6 +6,7 @@ type props = ViewProps & {
   text?: string;
   color: keyof typeof colors.text;
 };
+
 export function StylisedTitle({ text, color, ...rest }: props) {
   return (
     <View style={[styles.container, { borderLeftColor: color }]} {...rest}>
@@ -18,8 +19,11 @@ export function StylisedTitle({ text, color, ...rest }: props) {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: 180,
     borderLeftWidth: 1,
     paddingLeft: 10,
-    marginLeft: 10,
+    marginLeft: 10, // à supprimer
   },
 });
