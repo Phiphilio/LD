42d1938549a6535ad5016a0e86ffda2f9f6bdadc
paoue <grappercosmo@gmail.com>
2025-02-10 14:26:13 +0100

    je personnalise le texte

--- components/stylisedTitle.tsx
+++ components/stylisedTitle.tsx
@@ -1,16 +1,25 @@
-import { View, ViewProps } from "react-native";
+import { StyleSheet, View, ViewProps } from "react-native";
 import { ThemedText } from "./ThemedText";
-import { Children } from "react";
+import { colors } from "@/constants/color";
 
 type props = ViewProps & {
   text?: string;
+  color: keyof typeof colors.text;
 };
-export function StylisedTitle({ text, ...rest }: props) {
+export function StylisedTitle({ text, color, ...rest }: props) {
   return (
-    <View {...rest}>
-      <ThemedText variant="title" textColor="white">
+    <View style={[styles.container, { borderLeftColor: color }]} {...rest}>
+      <ThemedText variant="title" textColor={color}>
         {text}
       </ThemedText>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderLeftWidth: 1,
+    paddingLeft: 10,
+    marginLeft: 10,
+  },
+});
