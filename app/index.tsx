e7314ec3d24e2bb32912d693bfd58a18490ce0e7
paoue <grappercosmo@gmail.com>
2025-02-11 14:12:22 +0100

    j'aligne tout au centre

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,38 @@
+import { Card } from "@/components/card";
+import { CategorieCard } from "@/components/categorieCard";
+import { RecipeCard } from "@/components/recipeCard";
+import { StylisedTitle } from "@/components/stylisedTitle";
 import { colors } from "@/constants/color";
+import { useFetchData } from "@/hooks/useFecthData";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function index() {
   const backgroundColor = colors.background.glassPurple;
+  const { data, loading, error } = useFetchData(
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=p"
+  );
+
+  const drinkList = data ?? []; // Si `data` est null, on retourne un tableau vide
   return (
     <View style={[styles.container, { backgroundColor: backgroundColor }]}>
-      <Text>test</Text>
+      <StylisedTitle
+        text="POPULAR DISH OF THE WEEK"
+        color="whiteVariant"
+        style={styles.subtile}
+      />
+      <View style={styles.test}>
+        <RecipeCard
+          img={drinkList[9]?.strDrinkThumb ?? ""}
+          title={drinkList[9]?.strDrink ?? ""}
+          classification={drinkList[11]?.strAlcoholic ?? ""}
+        />
+      </View>
+      <StylisedTitle
+        text="CATEGORIES"
+        color="whiteVariant"
+        style={styles.subtile}
+      />
+      <CategorieCard />
     </View>
   );
 }
@@ -13,5 +40,14 @@ export default function index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+  subtile: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  test: {
+    marginLeft: 50,
   },
 });
