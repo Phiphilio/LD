8c40a30fe5ccb65fdbcfdb8aa7a8e8db24ea60d8
paoue <grappercosmo@gmail.com>
2025-02-17 12:51:46 +0100

    on entoure l'app du provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,25 @@
+import { RecetteFavProvider } from "@/constants/favorisContext";
 import { Tabs } from "expo-router";
-import { View } from "react-native";
-import PagerView from "react-native-pager-view";
 
 export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "index",
-        }}
-      />
-      <Tabs.Screen
-        name="stack"
-        options={{
-          href: null,
-        }}
-      />
-    </Tabs>
+    <RecetteFavProvider>
+      <Tabs
+        screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "index",
+          }}
+        />
+        <Tabs.Screen
+          name="stack"
+          options={{
+            href: null,
+          }}
+        />
+      </Tabs>
+    </RecetteFavProvider>
   );
 }
