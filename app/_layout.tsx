647b1d29500d24934646067b17b0748c493a560c
paoue <grappercosmo@gmail.com>
2025-02-17 12:59:45 +0100

    ajout de react-native-gesture-handler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,31 @@
-import { RecetteFavProvider } from "@/constants/favorisContext";
 import { Tabs } from "expo-router";
+import { RecetteFavProvider } from "@/constants/favorisContext";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <RecetteFavProvider>
-      <Tabs
-        screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "index",
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <RecetteFavProvider>
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: { display: "none" },
           }}
-        />
-        <Tabs.Screen
-          name="stack"
-          options={{
-            href: null,
-          }}
-        />
-      </Tabs>
-    </RecetteFavProvider>
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "index",
+            }}
+          />
+          <Tabs.Screen
+            name="stack"
+            options={{
+              href: null,
+            }}
+          />
+        </Tabs>
+      </RecetteFavProvider>
+    </GestureHandlerRootView>
   );
 }
